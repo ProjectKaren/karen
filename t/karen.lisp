2f4aa962788435b06049e577b4a389def7ffef64
tamamu <tamamu.1r1s@gmail.com>
2016-09-11 22:26:51 +0900

    write Hello world test

--- t/karen.lisp
+++ t/karen.lisp
@@ -9,7 +9,20 @@
 
 (plan nil)
 
+(defvar +test-html+
+  '(:html nil (
+    (:head nil (
+      (:meta ((:charset . "utf-8")) nil)
+      (:title nil ("Document"))
+    ))
+    (:body nil (
+      (:h1 nil ("Hello world!"))
+    ))
+  ))
+)
+
+(defparameter *test-element* (make-element +test-html+))
 (diag "Testing Gtk main...")
-(isnt (main) nil)
+(isnt (main *test-element*) nil)
 
 (finalize)
